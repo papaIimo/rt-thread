d9b431d27a8840c3d88fbf2a2e1db28183da9eb0
thread-liu <lk9608@outlook.com>
2021-04-14 17:29:13 +0800

    test

--- bsp/asm9260t/rtconfig.h
+++ bsp/asm9260t/rtconfig.h
@@ -8,10 +8,10 @@
 
 #define RT_NAME_MAX 8
 #define RT_ALIGN_SIZE 4
-#define RT_THREAD_PRIORITY_MAX 32
-#define RT_TICK_PER_SECOND 100
-#define RT_DEBUG
-#define RT_DEBUG_COLOR
+	#define RT_THREAD_PRIORITY_MAX 32
+	#define RT_TICK_PER_SECOND 100
+	#define RT_DEBUG
+	#define RT_DEBUG_COLOR
 #define RT_USING_OVERFLOW_CHECK
 #define RT_DEBUG_INIT 0
 /* RT_DEBUG_THREAD is not set */
